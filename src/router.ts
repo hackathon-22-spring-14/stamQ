e27d6e27e772c56518dd39031c0fc955110eccf0
kyosu-1 <sho013039@gmail.com>
2022-06-20 00:05:00 +0900

    test

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Create from './components/pages/CreatePage.vue'
 
-const routes = [{ path: '/', name: 'createPage', component: Create }]
+const routes = [{ path: '/stamQ', name: 'createPage', component: Create }]
 
 const router = createRouter({
   history: createWebHistory(), // HTML5 History モード
